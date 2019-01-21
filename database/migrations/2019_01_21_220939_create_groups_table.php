010c42a4d19045a91373afcaef77d71235f859dd
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2019-01-21 17:15:28 -0600

    Fixed down function

--- database/migrations/2019_01_21_220939_create_groups_table.php
+++ database/migrations/2019_01_21_220939_create_groups_table.php
@@ -38,6 +38,7 @@ class CreateGroupsTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('group_student');
         Schema::dropIfExists('groups');
     }
 }
