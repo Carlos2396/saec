1acdca7ca0400ece90c7dddbe683237601815764
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2019-01-23 12:19:59 -0600

    Competence and activity_competence migration completed

--- database/migrations/2019_01_21_221059_create_competences_table.php
+++ database/migrations/2019_01_21_221059_create_competences_table.php
@@ -15,6 +15,20 @@ class CreateCompetencesTable extends Migration
     {
         Schema::create('competences', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string ('description');
+            $table->enum('type', ['Type1', 'Type2']);
+            $table->string('professor_id');
+            $table->foreign('professor_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('activity_competence', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('activity_id')->unsigned();
+            $table->foreign('activity_id')->references('id')->on('activities')->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('competence_id')->unsigned();
+            $table->foreign('competence_id')->references('id')->on('competences')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
     }
